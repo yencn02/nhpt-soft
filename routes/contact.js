f7dd3bfbdb0715950615df993bb6f515c878f1de
Yen Ha <yencn02@gmail.com>
2015-10-10 01:20:34 +0700

    Send contact email

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
-
+var nodemailer = require('nodemailer')
 /* GET about page. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact'});
 });
 
+router.post('/send', function(req, res, next){
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth:{
+      user: 'yencn02@gmail.com',
+      pass: 'something'
+    }
+  });
+  var mailOptions = {
+    from: req.body.name + ' <'+req.body.email+'>',
+    to: 'yencn02@gmail.com',
+    subject: 'Website Submission',
+    text: 'You have a new submission with the following details...Name: ' + req.body.name+ ' Email: ' + req.body.email +' Message: ' + req.body.message,
+    html: '<p>You have a new submission with the following details</p><ul><li>Name: ' +req.body.name+ '</li><li>Email: ' +req.body.email+ '</li><li>Message: ' +req.body.message+ '</li></ul>'
+  }
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log(error)
+      res.redirect('/')
+    }else{
+      console.log('Message Sent: ' + info.response);
+      res.redirect('/')
+    }
+  })
+});
 module.exports = router;
