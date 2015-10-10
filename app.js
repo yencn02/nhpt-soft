628b2e253726de73555cf5a20f07f665771a6bc0
Yen Ha <yencn02@gmail.com>
2015-10-10 16:18:48 +0700

    Validate form, support upload file, show flash message

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer')
+var expressValidator = require('express-validator')
+var flash = require('connect-flash')
 
 var routes = require('./routes/index');
 var about = require('./routes/about');
@@ -22,8 +25,12 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('secret'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressValidator());
+app.use(session({cookie: { maxAge: 60000 }}));
+app.use(flash());
+
 
 app.use('/', routes);
 app.use('/about', about);
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,14 +11,67 @@ router.get('/', function(req, res, next) {
 
 router.get('/register', function(req, res, next) {
   res.render('users/register', {
-  	title: 'Register'
+    title: 'Register'
   });
+});
+
+router.post('/register', upload.single('avatar'), function(req, res, next) {
+  var name = req.body.name, email = req.body.email,
+      username = req.body.username, password = req.body.password,
+      password_confirmation = req.body.password_confirmation;
+  var avatar = null;
+  // Check for Image field
+  if(req.file){
+    avatar = req.file
+  }else{
+    // Set default image
+    avatar = 'noimage.png'
+  }
+  // Form validation
+  req.checkBody('name', 'Name field is required').notEmpty()
+  req.checkBody('email', 'Email field is required').notEmpty()
+  req.checkBody('email', 'Email not valid').isEmail()
+  req.checkBody('username', 'Username field is required').notEmpty()
+  req.checkBody('password', 'Password field is required').notEmpty()
+  req.checkBody('password_confirmation', 'Passwords do not match').equals(req.body.password)
+
+  // Check for errors
+  var errors = req.validationErrors();
+  console.log(errors);
+  if(errors){
+    res.render('users/register', {
+      errors: errors,
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      password_confirmation: password_confirmation
+    })
+  }else{
+    // var newUser = new User({
+    //   name: name,
+    //   email: email,
+    //   username: username,
+    //   password: password,
+    //   avatar: avatar
+    // });
+    // // Create user
+    // User.createUser(newUser, function(err, user){
+    //   if(err) throw err;
+    //   console.log(user);
+    // })
+
+    // Success Message
+    req.flash('success', 'You are now registered and may login')
+    res.redirect('/')
+  }
 });
 
 router.get('/login', function(req, res, next) {
   res.render('users/login', {
-  	title: 'Login'
+    title: 'Login'
   });
 });
+
 
 module.exports = router;
